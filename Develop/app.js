ca4c178d8de5f48df5259bc9b84241553d6bda18
Atul Mahajan <luta_atul@hotmail.com>
2020-04-06 23:35:56 +1000

    Inputting value from user for multiple employees.

--- Develop/app.js
+++ Develop/app.js
@@ -1,35 +1,108 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+class Getdetails {
+  constructor() {}
+
+  async askBasicinfo() {
+    try {
+      await inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "role",
+            message: "Enter role:",
+          },
+          {
+            type: "input",
+            name: "name",
+            message: "Enter name:",
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Enter email:",
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "Enter id:",
+          },
+        ])
+        .then((response) => {
+          this.role = response.role.toLowerCase();
+          this.extraDetail();
+        });
+    } catch (err) {
+      console.log("Input error!");
+    }
+  }
+
+  async extraDetail() {
+    try {
+      if (this.role === "manager") {
+        await inquirer.prompt({
+          type: "input",
+          name: "officeNumber",
+          message: "Enter Office Number:",
+        });
+      } else if (this.role === "engineer") {
+        await inquirer.prompt({
+          type: "input",
+          name: "githubuser",
+          message: "Enter Github username:",
+        });
+      } else if (this.role === "intern") {
+        await inquirer.prompt({
+          type: "input",
+          name: "school",
+          message: "Enter School name:",
+        });
+      }
+    } catch (err) {
+      console.log("Extra input error!");
+    }
+    this.nextEmployee();
+  }
+
+  async nextEmployee() {
+    try {
+      await inquirer
+        .prompt({
+          type: "confirm",
+          name: "more",
+          message: "Do you want add another employee:",
+        })
+        .then((response) => {
+          if (response.more) {
+            this.askBasicinfo();
+          } else {
+            this.quit();
+          }
+        });
+    } catch (err) {
+      console.log("More employee input error!");
+    }
+  }
+
+  // Logs goodbye and exits the node app
+  quit() {
+    console.log("\nGoodbye!");
+    process.exit(0);
+  }
+}
+
+const getDetails = new Getdetails();
+
+// Start
+getDetails.askBasicinfo();
